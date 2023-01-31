b90da3ac77a807218f582ee2e01522f9d07b1374
ruben97w33 <w33cuba@gmail.com>
2023-01-31 09:49:06 -0500

    change meta title

--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,7 @@ import Head from "next/head";
 export default function Header() {
   return (
     <Head>
-      <title>DeniasHome</title>
+      <title>DeniasHomeStory</title>
       <meta
         name="description"
         content="Photovoltaik Angebot - individuelle Solar-Beratung."
